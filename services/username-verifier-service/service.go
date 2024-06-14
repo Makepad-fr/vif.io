1d4e9a8a9913f30378438f16e43e0f301d53366e
kaanyagci <kaan.yagci@makepad.fr>
2024-06-14 13:51:55 +0200

    chore(services): setup database connection values from environment variables
    Signed-off-by: kaanyagci <kaan.yagci@makepad.fr>

--- services/username-verifier-service/service.go
+++ services/username-verifier-service/service.go
@@ -1,6 +1,36 @@
 package usernameverifierservice
 
-import "strings"
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/Makepad-fr/vif.io/model"
+)
+
+var dbURL, dbUsername, dbPassword string
+
+const dbURLEnvVarName = "DATABASE_URL"
+const dbUsernameEnvVarName = "DATABASE_USER"
+const dbPasswordEnvVarName = "DATABASE_PASSWORD"
+
+func init() {
+	v, exists := os.LookupEnv(dbURLEnvVarName)
+	if !exists {
+		log.Fatalf("Environment variable %s does not exists\n", dbURLEnvVarName)
+	}
+	dbURL = v
+	v, exists = os.LookupEnv(dbUsernameEnvVarName)
+	if !exists {
+		log.Fatalf("Environment variable %s does not exists\n", dbUsernameEnvVarName)
+	}
+	dbUsername = v
+	v, exists = os.LookupEnv(dbPasswordEnvVarName)
+	if !exists {
+		log.Fatalf("Environment variable %s does not exists\n", dbPasswordEnvVarName)
+	}
+	dbPassword = v
+}
 
 // isTrimmedEmpty returns true if the given string is empty after trimmed
 func isTrimmedEmpty(s string) bool {
